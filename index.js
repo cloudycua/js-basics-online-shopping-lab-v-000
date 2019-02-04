eb5dec50f1e6eba0dc997a056d00d56f0fed9398
Christine Cua <cloudycua@gmail.com>
2019-02-04 21:37:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ function total() {
 
 function removeFromCart(item) {
   for (var i in cart) {
-
-    if (cart[i].itemName == item) {
 debugger
+    if (cart[i].itemName == item) {
+
       cart.splice(i, 1)
 
     }
